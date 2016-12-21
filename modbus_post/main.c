a9ddc01fb6544bcd277ed25a60f192274c554f3c
Michael Koppelman <michael@clockwork.net>
2016-12-21 16:11:40 -0600

    Initial commit

--- modbus_post/main.c
+++ modbus_post/main.c
@@ -7,9 +7,28 @@
 //
 
 #include <stdio.h>
+#include <modbus/modbus.h>
+#include <unistd.h>
+#include <time.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main( int argc, char** argv ) {
+    while( 1 ) {
+        modbus_t	*mb;
+        uint16_t	tab_reg[32];
+        
+        mb = modbus_new_tcp( "10.0.1.128", 502 );
+        modbus_connect( mb );
+        
+        int rc = modbus_read_input_registers( mb, 0, 11, tab_reg );
+        if ( rc == -1 ) {
+            fprintf( stderr, "Error: %i, %s\n", 1, modbus_strerror(1) );
+        }
+        else {
+            fprintf( stdout, "%li, %f, %f\n", time(0), modbus_get_float_badc( &tab_reg[6] ), modbus_get_float_badc( &tab_reg[8] ) );
+            fflush(0);
+        }
+        modbus_close( mb );
+        modbus_free( mb );
+        sleep(10);
+    }
 }
