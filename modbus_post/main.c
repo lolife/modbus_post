4587cd2a3cf6c90ef4db67f16cc5164d1e53deb4
Michael Koppelman <michael@clockwork.net>
2016-12-22 08:26:12 -0600

    It works!

--- modbus_post/main.c
+++ modbus_post/main.c
@@ -10,18 +10,20 @@
 #include <modbus/modbus.h>
 #include <unistd.h>
 #include <time.h>
+#include <errno.h>
 
 int main( int argc, char** argv ) {
     while( 1 ) {
         modbus_t	*mb;
         uint16_t	tab_reg[32];
-        
+
         mb = modbus_new_tcp( "10.0.1.128", 502 );
+        //    modbus_set_debug( mb, TRUE );
         modbus_connect( mb );
-        
+
         int rc = modbus_read_input_registers( mb, 0, 11, tab_reg );
         if ( rc == -1 ) {
-            fprintf( stderr, "Error: %i, %s\n", 1, modbus_strerror(1) );
+            fprintf( stderr, "Error: %i, %s\n", errno, modbus_strerror(errno) );
         }
         else {
             fprintf( stdout, "%li, %f, %f\n", time(0), modbus_get_float_badc( &tab_reg[6] ), modbus_get_float_badc( &tab_reg[8] ) );
@@ -29,6 +31,6 @@ int main( int argc, char** argv ) {
         }
         modbus_close( mb );
         modbus_free( mb );
-        sleep(10);
+        sleep(30);
     }
 }
