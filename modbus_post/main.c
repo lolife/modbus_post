d5b255a61b4165586429df456469e450e5a25dba
Michael Koppelman <michael@clockwork.net>
2016-12-22 18:06:46 -0600

    Minor tweakin'

--- modbus_post/main.c
+++ modbus_post/main.c
@@ -17,20 +17,21 @@ int main( int argc, char** argv ) {
         modbus_t	*mb;
         uint16_t	tab_reg[32];
 
-        mb = modbus_new_tcp( "10.0.1.128", 502 );
+//        mb = modbus_new_tcp( "10.0.1.128", 502 );
+        mb = modbus_new_tcp( "10.0.0.32", 502 );
         //    modbus_set_debug( mb, TRUE );
         modbus_connect( mb );
 
-        int rc = modbus_read_input_registers( mb, 0, 11, tab_reg );
+        int rc = modbus_read_input_registers( mb, 0, 12, tab_reg );
         if ( rc == -1 ) {
             fprintf( stderr, "Error: %i, %s\n", errno, modbus_strerror(errno) );
         }
         else {
-            fprintf( stdout, "%li, %f, %f\n", time(0), modbus_get_float_badc( &tab_reg[6] ), modbus_get_float_badc( &tab_reg[8] ) );
-            fflush(0);
+            fprintf( stdout, "%li, %f, %f, %f\n", time(0), modbus_get_float_badc( &tab_reg[6] ), modbus_get_float_badc( &tab_reg[10] ), modbus_get_float_badc( &tab_reg[8 ] ) );
         }
+        fflush(0);
         modbus_close( mb );
         modbus_free( mb );
-        sleep(30);
+        sleep(15);
     }
 }
