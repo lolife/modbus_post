851c26c7c6ff7a56a26f6423dcbb950de699fefb
Michael Koppelman <michael@clockwork.net>
2016-12-27 11:59:00 -0600

    Added more fields to report.

--- modbus_post/main.c
+++ modbus_post/main.c
@@ -16,9 +16,8 @@ int main( int argc, char** argv ) {
     while( 1 ) {
         modbus_t	*mb;
         uint16_t	tab_reg[32];
-
-//        mb = modbus_new_tcp( "10.0.1.128", 502 );
-        mb = modbus_new_tcp( "10.0.0.32", 502 );
+        mb = modbus_new_tcp( "10.0.1.128", 502 );
+//        mb = modbus_new_tcp( "10.0.0.32", 502 );
         //    modbus_set_debug( mb, TRUE );
         modbus_connect( mb );
 
@@ -27,11 +26,14 @@ int main( int argc, char** argv ) {
             fprintf( stderr, "Error: %i, %s\n", errno, modbus_strerror(errno) );
         }
         else {
-            fprintf( stdout, "%li, %f, %f, %f\n", time(0), modbus_get_float_badc( &tab_reg[6] ), modbus_get_float_badc( &tab_reg[10] ), modbus_get_float_badc( &tab_reg[8 ] ) );
+            fprintf( stdout, "%li, %0.3f, %0.3f, %0.3f, %0.3f, %0.3f, %0.3f\n",
+                    time(0), modbus_get_float_badc(&tab_reg[0]), modbus_get_float_badc(&tab_reg[2]),
+                    modbus_get_float_badc( &tab_reg[4] ), modbus_get_float_badc( &tab_reg[6] ),
+                    modbus_get_float_badc( &tab_reg[8] ), modbus_get_float_badc( &tab_reg[10] ) );
         }
         fflush(0);
         modbus_close( mb );
         modbus_free( mb );
-        sleep(15);
+        sleep(11);
     }
 }
